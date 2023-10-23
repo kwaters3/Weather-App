2b5bca2a4ec60bf76f5d23c4afd222e141be179f
kwaters3 <knickler3@gmail.com>
2023-10-23 16:58:58 -0400

    updated javscript

--- Assets/script.js
+++ Assets/script.js
@@ -4,83 +4,74 @@ const cityList = document.getElementById('city-list');
 // Check if there is a stored city name in localStorage
 const storedCity = localStorage.getItem('city');
 if (storedCity) {
-    getWeatherData(storedCity);
+  getWeatherData(storedCity);
 }
 
 document.getElementById('city-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const cityNameInput = document.getElementById('city-input');
+  event.preventDefault();
+  const cityNameInput = document.getElementById('city-input');
+  const cityName = cityNameInput.value;
+  cityNameInput.value = '';
 
-    console.log(cityNameInput); // Debugging line to check the input element
-
-    const cityName = cityNameInput.value;
-    cityNameInput.value = '';
-
-    getWeatherData(cityName);
-})
+  getWeatherData(cityName);
+});
 
 cityList.addEventListener('click', function(event) {
-    if (event.target.matches('li')) {
-        getWeatherData(event.target.textContent);
-    }
+  if (event.target.matches('li')) {
+    getWeatherData(event.target.textContent);
+  }
 });
 
 async function getWeatherData(cityName) {
-    localStorage.setItem('city', cityName);
+  localStorage.setItem('city', cityName);
 
-    try {
-        const currentResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`);
+  try {
+    const currentResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`);
+    const currentData = await currentResponse.json();
+    const coordinates = currentData.coord;
+    const lat = coordinates.lat;
+    const lon = coordinates.lon;
+    const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    const forecastData = await forecastResponse.json();
 
-const currentData = await currentResponse.json();
-const coordinates = currentData.coord;
-const lat = coordinates.lat;
-const lon = coordinates.lon;
-const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
-const forecastData = await forecastResponse.json();
-
-        displayWeatherData(currentData, forecastData);
-        addToSearchHistory(cityName);
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    displayWeatherData(currentData, forecastData);
+    addToSearchHistory(cityName);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
-
-
 
 // Adds to the search history list
 function addToSearchHistory(cityName) {
   const cityListItem = document.createElement('li');
   cityListItem.textContent = cityName;
   cityList.appendChild(cityListItem);
-};
-
-
-
-
+}
 
 // function to display the weather data
 function displayWeatherData(currentData, forecastData) {
   const weatherInfo = document.getElementById('weather-info');
 
   // Current weather information
-const temperatureKelvin = currentData.main.temp;
-const temperatureCelsius = temperatureKelvin - 273.15;
-const temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;     
-    console.log('Temperature in Celsius:', temperatureCelsius);
-  console.log('Temperature in Fahrenheit:', temperatureFahrenheit);
-const windSpeedMetersPerSecond = currentData.wind.speed;
-const windSpeedMph = windSpeedMetersPerSecond * 2.23694; // Convert wind speed to mph
+  const temperatureKelvin = currentData.main.temp;
+  const temperatureCelsius = temperatureKelvin - 273.15;
+  const temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+  const windSpeedMetersPerSecond = currentData.wind.speed;
+  const windSpeedMph = windSpeedMetersPerSecond * 2.23694;
+
+  // Get the current date
+  const currentDate = new Date();
+  const formattedDate = currentDate.toDateString();
 
   const currentWeatherHTML = `
     <div class="current-weather">
       <h2>${currentData.name}</h2>
-      <p class="date">Today</p>
-      <p><b>Temperature: </b> ${temperatureFahrenheit.toFixed(2)}°F</p> <!-- Display temperature in Fahrenheit -->
+      <p class="date1">Today: ${formattedDate}</p>
+      <p><b>Temperature: </b> ${temperatureFahrenheit.toFixed(2)}°F</p>
       <p><b>Humidity:</b> ${currentData.main.humidity}%</p>
-      <p><b>Wind Speed:</b> ${windSpeedMph.toFixed(2)} mph</p> <!-- Display wind speed in mph -->
-      </div> 
-
-      `;
+      <p><b>Wind Speed:</b> ${windSpeedMph.toFixed(2)} mph</p>
+    </div>
+  `;
 
   // Forecast data for the next 5 days
   const forecastWeatherHTML = `
@@ -94,31 +85,30 @@ const windSpeedMph = windSpeedMetersPerSecond * 2.23694; // Convert wind speed t
 
   // Calculate and display the weather data for the next 5 days with specific dates
   const forecastContainer = document.getElementById('forecast-container');
-  const currentDate = new Date(currentData.dt * 1000);
+
   for (let i = 1; i < 6; i++) {
-      const forecastIndex = i * 7;
-      const day = forecastData.list[forecastIndex];
-      const forecastDate = new Date(day.dt * 1000);
-      const iconURL = `https://openweathermap.org/img/w/${day.weather[0].icon}.png`;
+    const forecastIndex = i * 7;
+    const day = forecastData.list[forecastIndex];
+    const forecastDate = new Date(day.dt * 1000);
+    const iconURL = `https://openweathermap.org/img/w/${day.weather[0].icon}.png`;
 
-
-    const temperatureKelvinForecast = currentData.main.temp;
+    const temperatureKelvinForecast = day.main.temp;
     const temperatureCelsiusForecast = temperatureKelvinForecast - 273.15;
-    const temperatureFahrenheitForecast = (temperatureCelsiusForecast * 9/5) + 32;   
-      const windSpeedMetersPerSecondForecast = day.wind.speed;
-      const windSpeedMphForecast = windSpeedMetersPerSecondForecast * 2.23694;
+    const temperatureFahrenheitForecast = (temperatureCelsiusForecast * 9/5) + 32;
+    const windSpeedMetersPerSecondForecast = day.wind.speed;
+    const windSpeedMphForecast = windSpeedMetersPerSecondForecast * 2.23694;
 
-      const forecastDayHTML = `
-          <div class="forecast-card">
-          <div class="cardBack">
-              <p class="date">${forecastDate.toDateString()}</p>
-              <img src="${iconURL}" alt="${day.weather[0].description}">
-              <p><b>Temperature:</b> ${temperatureFahrenheitForecast.toFixed(2)}°F</p>
-              <p><b>Humidity:</b> ${day.main.humidity}%</p>
-              <p><b>Wind Speed:</b> ${windSpeedMphForecast.toFixed(2)} mph</p>
-          </div>
-            </div>
-      `;
-      forecastContainer.innerHTML += forecastDayHTML;
+    const forecastDayHTML = `
+      <div class="forecast-card">
+        <div class="cardBack">
+          <p class="date">${forecastDate.toDateString()}</p>
+          <img src="${iconURL}" alt="${day.weather[0].description}">
+          <p><b>Temperature:</b> ${temperatureFahrenheitForecast.toFixed(2)}°F</p>
+          <p><b>Humidity:</b> ${day.main.humidity}%</p>
+          <p><b>Wind Speed:</b> ${windSpeedMphForecast.toFixed(2)} mph</p>
+        </div>
+      </div>
+    `;
+    forecastContainer.innerHTML += forecastDayHTML;
   }
 }
